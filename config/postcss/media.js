b2ae0db448b26b1de3dfeec154f5b1e38928ca15
Alexey Ivanov <mail@alexeyivanov.info>
2019-10-24 23:31:34 +0900

    Update breakpoints

--- config/postcss/media.js
+++ config/postcss/media.js
@@ -1,13 +1,12 @@
 const screens = {
   desktop: 1170,
-  tablet: 768,
   phone: 376
 };
 
 module.exports = {
   customMedia: {
     "--sm-scr": `(max-width: ${screens.phone}px)`,
-    "--md-scr": `(max-width: ${screens.tablet - 1}px)`,
+    "--md-scr": `(max-width: ${screens.desktop - 1}px)`,
     "--lg-scr": `(min-width: ${screens.desktop}px)`
   }
 };
