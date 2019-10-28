db2944b3f4d866c41d6af3acf85abab457dd4cc8
Alexey Ivanov <mail@alexeyivanov.info>
2019-10-28 23:14:41 +0900

    Fix subscribe form for mobile

--- src/components/subscribe/index.tsx
+++ src/components/subscribe/index.tsx
@@ -3,15 +3,15 @@ import React from "react";
 
 import styles from "./styles.module.css";
 
-import { ReactComponent as Glyph1 } from "./glyph-1.svg";
-import { ReactComponent as Glyph2 } from "./glyph-2.svg";
+import { default as Glyph1Src } from "./glyph-1.svg";
+import { default as Glyph2Src } from "./glyph-2.svg";
 
 import SubscribeForm from "./form";
 
 export default function Subscribe() {
   return (
     <div className={styles.wrapper}>
-      <Glyph1 className={cn(styles.glyph, styles.topleft)} />
+      <img src={Glyph1Src} className={cn(styles.glyph, styles.topleft)} />
       <div className={styles.container}>
         <div className={styles.title}>
           Subscribe for updates. We won&#39;t spam you.
@@ -20,7 +20,7 @@ export default function Subscribe() {
           <SubscribeForm />
         </div>
       </div>
-      <Glyph2 className={cn(styles.glyph, styles.rigthbottom)} />
+      <img src={Glyph2Src} className={cn(styles.glyph, styles.rigthbottom)} />
     </div>
   );
 }
