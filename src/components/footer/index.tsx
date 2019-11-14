4c20a5e1cd95003981908c0815accf42ceb4605f
Alexey Ivanov <mail@alexeyivanov.info>
2019-11-14 18:29:35 +0900

    Replace footer logo image with vsg

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { footerMenuItems, siteLinks } from "../../data";
 
 import IconSet from "../icon-set";
 
-import logoSrc from "./icons/logo_white.png";
+import { ReactComponent as LogoSVG } from "./logo.svg";
 import styles from "./styles.module.css";
 
 interface IFooterProps {
@@ -16,8 +16,8 @@ function Footer({ onClick }: IFooterProps) {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <div className={styles.top}>
-          <a className={styles.logo} href={siteLinks.root}>
-            <img src={logoSrc} alt="dvc.org" width={36} height={23} />
+          <a className={styles.logo} href={siteLinks.root} title="dvc.org">
+            <LogoSVG />
           </a>
         </div>
         <div className={styles.columns}>
