3e10a80fab8965a0e44e2373bb008df1184c5fcf
Alexey Ivanov <mail@alexeyivanov.info>
2019-11-20 22:29:57 +0900

    Fix error in analytics config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,89 +7,71 @@ const netlifyCMSPathConfig = {
   }
 };
 
-const GAConfig =
-  process.env.CONTEXT === "production"
-    ? {
-        resolve: "gatsby-plugin-google-analytics",
-        options: {
-          trackingId: process.env.GA_ID,
-          respectDNT: true
-        }
-      }
-    : {};
-
 const title = "Data Version Control";
 
-module.exports = {
-  siteMetadata: {
-    title,
-    description:
-      "Every month we are sharing here our news, findings, interesting reads, community takeaways, and everything along the way.",
-    siteUrl: "https://blog.dvc.org"
+const plugins = [
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      path: path.join(__dirname, "content", "blog"),
+      name: "blog"
+    }
   },
-  plugins: [
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: path.join(__dirname, "content", "blog"),
-        name: "blog"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: path.join(__dirname, "content", "authors"),
-        name: "authors"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: path.join(__dirname, "content", "assets"),
-        name: "assets"
-      }
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: path.join(__dirname, "static", "uploads"),
-        name: "images"
-      }
-    },
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              maxWidth: 700
-            }
-          },
-          "gatsby-remark-responsive-iframe",
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants",
-          netlifyCMSPathConfig
-        ]
-      }
-    },
-    "gatsby-plugin-typescript",
-    "gatsby-plugin-postcss",
-    {
-      resolve: "gatsby-plugin-svgr",
-      options: {
-        ref: true
-      }
-    },
-    "gatsby-plugin-tslint",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      path: path.join(__dirname, "content", "authors"),
+      name: "authors"
+    }
+  },
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      path: path.join(__dirname, "content", "assets"),
+      name: "assets"
+    }
+  },
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      path: path.join(__dirname, "static", "uploads"),
+      name: "images"
+    }
+  },
+  {
+    resolve: "gatsby-transformer-remark",
+    options: {
+      plugins: [
+        {
+          resolve: "gatsby-remark-images",
+          options: {
+            maxWidth: 700
+          }
+        },
+        "gatsby-remark-responsive-iframe",
+        "gatsby-remark-prismjs",
+        "gatsby-remark-copy-linked-files",
+        "gatsby-remark-smartypants",
+        netlifyCMSPathConfig
+      ]
+    }
+  },
+  "gatsby-plugin-typescript",
+  "gatsby-plugin-postcss",
+  {
+    resolve: "gatsby-plugin-svgr",
+    options: {
+      ref: true
+    }
+  },
+  "gatsby-plugin-tslint",
+  "gatsby-transformer-sharp",
+  "gatsby-plugin-sharp",
 
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
+  {
+    resolve: `gatsby-plugin-feed`,
+    options: {
+      query: `
           {
             site {
               siteMetadata {
@@ -101,21 +83,21 @@ module.exports = {
             }
           }
         `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description:
-                    edge.node.descriptionLong || edge.node.descriptionLong,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }]
-                });
+      feeds: [
+        {
+          serialize: ({ query: { site, allMarkdownRemark } }) => {
+            return allMarkdownRemark.edges.map(edge => {
+              return Object.assign({}, edge.node.frontmatter, {
+                description:
+                  edge.node.descriptionLong || edge.node.descriptionLong,
+                date: edge.node.frontmatter.date,
+                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                custom_elements: [{ "content:encoded": edge.node.html }]
               });
-            },
-            query: `
+            });
+          },
+          query: `
               {
                 allMarkdownRemark(
                   sort: { fields: [frontmatter___date], order: DESC }
@@ -138,27 +120,45 @@ module.exports = {
                 }
               }
             `,
-            output: "/rss.xml",
-            title
-          }
-        ]
-      }
-    },
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        name: "dvc.org",
-        short_name: "dvc.org",
-        start_url: "/",
-        background_color: "#eff4f8",
-        theme_color: "#eff4f8",
-        display: "minimal-ui",
-        icon: "static/512.png"
-      }
-    },
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-netlify-cms",
-    netlifyCMSPathConfig,
-    GAConfig
-  ]
+          output: "/rss.xml",
+          title
+        }
+      ]
+    }
+  },
+  {
+    resolve: "gatsby-plugin-manifest",
+    options: {
+      name: "dvc.org",
+      short_name: "dvc.org",
+      start_url: "/",
+      background_color: "#eff4f8",
+      theme_color: "#eff4f8",
+      display: "minimal-ui",
+      icon: "static/512.png"
+    }
+  },
+  "gatsby-plugin-react-helmet",
+  "gatsby-plugin-netlify-cms",
+  netlifyCMSPathConfig
+];
+
+if (process.env.CONTEXT === "production") {
+  plugins.push({
+    resolve: "gatsby-plugin-google-analytics",
+    options: {
+      trackingId: process.env.GA_ID,
+      respectDNT: true
+    }
+  });
+}
+
+module.exports = {
+  siteMetadata: {
+    title,
+    description:
+      "Every month we are sharing here our news, findings, interesting reads, community takeaways, and everything along the way.",
+    siteUrl: "https://blog.dvc.org"
+  },
+  plugins
 };
