d5bdd877f78798c965ca93833e9c675872754e8b
Alexey Ivanov <mail@alexeyivanov.info>
2019-12-18 20:56:19 +0900

    Add json withe latest post to use on community page

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,12 @@
-const path = require(`path`);
-const { createFilePath } = require(`gatsby-source-filesystem`);
+const fs = require("fs");
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
+const { siteMetadata } = require("./gatsby-config");
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
-  const blogPost = path.resolve(`./src/templates/blog-post.tsx`);
+  const blogPost = path.resolve("./src/templates/blog-post.tsx");
   const result = await graphql(
     `
       {
@@ -54,15 +56,72 @@ exports.createPages = async ({ graphql, actions }) => {
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions;
 
-  if (node.internal.type === `MarkdownRemark`) {
+  if (node.internal.type === "MarkdownRemark") {
     const value = createFilePath({ node, getNode }).replace(
       /^\/[0-9\-]*/,
       "/blog/"
     );
     createNodeField({
-      name: `slug`,
+      name: "slug",
       node,
       value
     });
   }
+};
+
+// Create json to use on https://dvc.org/community
+
+exports.onPostBuild = async function({ graphql }) {
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { fileAbsolutePath: { regex: "/content/blog/" } }
+        limit: 3
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              date
+              commentsUrl
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    throw new Error(result.errors);
+  }
+
+  const posts = result.data.allMarkdownRemark.edges.map(
+    ({
+      node: {
+        fields: { slug },
+        frontmatter: { title, date, commentsUrl }
+      }
+    }) => {
+      const url = `${siteMetadata.siteUrl}/${slug}`;
+
+      return {
+        url,
+        title,
+        date,
+        commentsUrl
+      };
+    }
+  );
+
+  const dir = path.join(__dirname, "/public/api");
+  const filepath = path.join(dir, "posts.js");
+
+  // Write json file to the public dir,
+  // it will be used community page later
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+  fs.writeFileSync(filepath, JSON.stringify(posts));
 };
