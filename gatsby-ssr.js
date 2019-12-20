6dcd0015bff067b096c69c7a0307893c12931fb6
Evgeniy Nagalskiy <evgeniy.nagalskiy@gmail.com>
2019-12-20 15:09:08 +0200

    refactor

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,18 +3,16 @@ var React = require("react")
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents()
   replaceHeadComponents([
-    headComponents,
-    <React.Fragment key="head-components">
-      <script
-        async
-        src="//embed.redditmedia.com/widgets/platform.js"
-        charSet="UTF-8"
-      ></script>
-      <script
-        async
-        src="https://platform.twitter.com/widgets.js"
-        charSet="utf-8"
-      ></script>
-    </React.Fragment>
+    ...headComponents,
+    <script
+      async
+      src="//embed.redditmedia.com/widgets/platform.js"
+      charSet="UTF-8"
+    />,
+    <script
+      async
+      src="https://platform.twitter.com/widgets.js"
+      charSet="utf-8"
+    />
   ])
 }
