cd9421f4c052b403abed2625097b7cd83e63a056
Alexey Ivanov <mail@alexeyivanov.info>
2020-01-10 18:12:11 +0900

    Remove unused deps

--- config/netlify/index.js
+++
@@ -1,8 +0,0 @@
-import CMS from 'netlify-cms-app';
-
-function Wrapper({ widgetFor }) {
-  return widgetFor('body');
-}
-
-CMS.registerPreviewStyle('/admin/styles.css');
-CMS.registerPreviewTemplate('blog', Wrapper);
